36a921d443a153613e7af42e555f42ab71d4a5ee
Devin Broussard <devin.ray.broussard@gmail.com>
2021-08-27 11:55:59 -0500

    added ref

--- Fight Simulation/Game.cs
+++ Fight Simulation/Game.cs
@@ -11,39 +11,44 @@ namespace Fight_Simulation
             //created monster one object, and gave its stats
             Monster monsterOne;
             monsterOne.name = "Wompus";
-            monsterOne.health = 5.0f;
-            monsterOne.attack = 10.0f;
+            monsterOne.health = 10.0f;
+            monsterOne.attack = 5.0f;
             monsterOne.defense = 5.0f;
 
             //Created monster two object, and gave its stats
             Monster monsterTwo;
             monsterTwo.name = "Thwompus";
-            monsterTwo.health = 5.0f;
+            monsterTwo.health = 20.0f;
             monsterTwo.attack = 10.0f;
             monsterTwo.defense = 5.0f;
 
             //Prints monster1 stats
             PrintStats(monsterOne);
-            Console.ReadKey();
-            Console.Clear();
 
             //Prints monster2 stats
             PrintStats(monsterTwo);
-            Console.ReadKey();
-            Console.Clear();
 
             //Monster 1 attacks monster 2
-            float damageTaken = CalculateDamage(monsterOne, monsterTwo);
-            monsterTwo.health -= damageTaken;
-            Console.WriteLine(monsterTwo.name + " has taken " + damageTaken);
-            Console.WriteLine();
-            Console.ReadKey();
+            float damageTaken = Fight(ref monsterOne, ref monsterTwo);
+            Console.Write(monsterTwo.name + " has taken " + damageTaken);
+            Console.WriteLine(" damage!");
 
             //Monster 2 attack monster 1
-            damageTaken = CalculateDamage(monsterOne, monsterTwo);
-            monsterOne.health -= damageTaken;
-            Console.WriteLine(monsterOne.name + " has taken " + damageTaken);
-            Console.ReadKey();
+            damageTaken = Fight(ref monsterTwo, ref monsterOne);
+            Console.Write(monsterOne.name + " has taken " + damageTaken);
+            Console.WriteLine(" damage!");
+
+            //prints stats
+            PrintStats(monsterOne);
+            PrintStats(monsterTwo);
+        }
+
+        float Fight(ref Monster attack, ref Monster defender)
+        {
+            float damageTaken = CalculateDamage(attack, defender);
+            defender.health -= damageTaken;
+
+            return damageTaken;
         }
 
         void PrintStats(Monster monster)
@@ -53,6 +58,17 @@ namespace Fight_Simulation
             Console.WriteLine("Attack: " + monster.attack);
             Console.WriteLine("Defense: " + monster.defense);
         }
+
+        float CalculateDamage(float attack, float defense)
+        {
+            float damage = attack - defense;
+            if (damage <= 0)
+            {
+                damage = 0;
+            }
+            return damage;
+        }
+
         float CalculateDamage(Monster attacker, Monster defender)
         {
             float damage = attacker.attack - defender.defense;
