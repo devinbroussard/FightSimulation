5b55d12699ff502b3893782c29946bf19327f1a4
Devin Broussard <devin.ray.broussard@gmail.com>
2021-09-01 16:57:25 -0500

    Updated fighting system

--- Fight Simulation/Game.cs
+++ Fight Simulation/Game.cs
@@ -4,46 +4,134 @@ using System.Text;
 
 namespace Fight_Simulation
 {
+    public struct Monster
+    {
+        public string name;
+        public float health;
+        public float attack;
+        public float defense;
+    }
+
     class Game
     {
-        public void Run()
+        Monster wompus;
+        Monster thwompus;
+        Monster backupWompus;
+        Monster unclePhil;
+        bool gameOver = false;
+        Monster currentMonsterOne;
+        Monster currentMonsterTwo;
+        int currentMonsterIndex = 1;
+
+        void Battle()
         {
-            //created monster one object, and gave its stats
-            Monster monsterOne;
-            monsterOne.name = "Wompus";
-            monsterOne.health = 10.0f;
-            monsterOne.attack = 5.0f;
-            monsterOne.defense = 5.0f;
-
-            //Created monster two object, and gave its stats
-            Monster monsterTwo;
-            monsterTwo.name = "Thwompus";
-            monsterTwo.health = 20.0f;
-            monsterTwo.attack = 10.0f;
-            monsterTwo.defense = 5.0f;
-
-            //Prints monster1 stats
-            PrintStats(monsterOne);
-
-            //Prints monster2 stats
-            PrintStats(monsterTwo);
+            //prints stats
+            PrintStats(currentMonsterOne);
+            PrintStats(currentMonsterTwo);
+            Console.ReadKey(true);
+            Console.Clear();
 
             //Monster 1 attacks monster 2
-            float damageTaken = Fight(ref monsterOne, ref monsterTwo);
-            Console.Write(monsterTwo.name + " has taken " + damageTaken);
+            float damageTaken = Fight(currentMonsterOne, ref currentMonsterTwo);
+            Console.Write(currentMonsterTwo.name + " has taken " + damageTaken);
             Console.WriteLine(" damage!");
 
             //Monster 2 attack monster 1
-            damageTaken = Fight(ref monsterTwo, ref monsterOne);
-            Console.Write(monsterOne.name + " has taken " + damageTaken);
-            Console.WriteLine(" damage!");
-
-            //prints stats
-            PrintStats(monsterOne);
-            PrintStats(monsterTwo);
+            damageTaken = Fight(currentMonsterTwo, ref currentMonsterOne);
+            Console.Write(currentMonsterOne.name + " has taken " + damageTaken);
+            Console.WriteLine(" damage!\n");
+            Console.ReadKey(true);
+            Console.Clear();
         }
 
-        float Fight(ref Monster attack, ref Monster defender)
+        public void Run()
+        {
+            //created monster one object, and gave its stats
+            wompus.name = "Wompus";
+            wompus.health = 10.0f;
+            wompus.attack = 5.0f;
+            wompus.defense = 5.0f;
+
+            //Created monster two object, and gave its stats
+            thwompus.name = "Thwompus";
+            thwompus.health = 20.0f;
+            thwompus.attack = 10.0f;
+            thwompus.defense = 5.0f;
+
+            backupWompus.name = "Backup Wompus";
+            backupWompus.attack = 25.6f;
+            backupWompus.defense = 5.0f;
+            backupWompus.health = 3.0f;
+
+            unclePhil.name = "Uncle Phil";
+            unclePhil.attack = 50.0f;
+            unclePhil.defense = 20.0f;
+            unclePhil.health = 50.0f;
+
+        }
+
+        Monster GetMonster (int monsterIndex)
+        {
+            Monster monster;
+            monster.name = "None";
+
+            if (monsterIndex == 1)
+                monster = unclePhil;
+            else if (monsterIndex == 2)
+                monster = backupWompus;
+            else if (monsterIndex == 3)
+                monster = wompus;
+            else if (monsterIndex == 4)
+                monster = thwompus;
+
+            return monster;
+        }
+
+        void Update()
+        {
+            Battle();
+        }
+
+        void UpdateCurrentMonsters()
+        {
+        }
+
+        string StartBattle(ref Monster monsterOne, ref Monster monsterTwo)
+        {
+            string battleResults = "No Contest";
+
+            while (monsterOne.health > 0 && monsterTwo.health > 0)
+            {
+                //prints stats
+                PrintStats(monsterOne);
+                PrintStats(monsterTwo);
+                Console.ReadKey(true);
+                Console.Clear();
+
+                //Monster 1 attacks monster 2
+                float damageTaken = Fight(monsterOne, ref monsterTwo);
+                Console.Write(monsterTwo.name + " has taken " + damageTaken);
+                Console.WriteLine(" damage!");
+
+                //Monster 2 attack monster 1
+                damageTaken = Fight(monsterTwo, ref monsterOne);
+                Console.Write(monsterOne.name + " has taken " + damageTaken);
+                Console.WriteLine(" damage!\n");
+                Console.ReadKey(true);
+                Console.Clear();
+            }
+
+            if (monsterOne.health <= 0 && monsterTwo.health <= 0) 
+                battleResults = "Draw";
+            else if (monsterOne.health > 0) 
+                battleResults = monsterOne.name;
+            else if (monsterTwo.health > 0)
+                battleResults = monsterTwo.name;
+
+            return battleResults;
+        }
+
+        float Fight(Monster attack, ref Monster defender)
         {
             float damageTaken = CalculateDamage(attack, defender);
             defender.health -= damageTaken;
@@ -57,6 +145,7 @@ namespace Fight_Simulation
             Console.WriteLine("Health: " + monster.health);
             Console.WriteLine("Attack: " + monster.attack);
             Console.WriteLine("Defense: " + monster.defense);
+            Console.WriteLine();
         }
 
         float CalculateDamage(float attack, float defense)
@@ -78,12 +167,5 @@ namespace Fight_Simulation
             }
             return damage;
         }
-    }
-    public struct Monster
-    {
-        public string name;
-        public float health;
-        public float attack;
-        public float defense;
     }
 }
